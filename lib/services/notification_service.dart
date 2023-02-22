1f17019f65a3f22c60a8f4225b938e5878e93a8b
Jordancbo Eftimov <ejordancho@gmail.com>
2023-02-22 22:53:18 +0100

    local notif

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationsServices {
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  final AndroidInitializationSettings _androidInitializationSettings =
+      const AndroidInitializationSettings('@android:drawable/ic_dialog_info');
+
+  void initialiseNotificaitons() async {
+    InitializationSettings initializationSettings =
+        InitializationSettings(android: _androidInitializationSettings);
+
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  void sendNotifications(String title, String body) async {
+    AndroidNotificationDetails androidNotificationDetails =
+        const AndroidNotificationDetails(
+      'channelId',
+      'channelName',
+      importance: Importance.max,
+      priority: Priority.max,
+    );
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+    );
+    await _flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      notificationDetails,
+    );
+  }
+}
